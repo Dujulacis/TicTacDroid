c19df368c29ed413ce7ad38fa8f6780fa173d994
Nils Tīdemanis <59266348+Dujulacis@users.noreply.github.com>
2025-04-18 18:00:05 +0300

    Create base playing space
    Create game grid using styles

--- app/src/main/java/dujulacis/tictacdroid/MainActivity.kt
+++ app/src/main/java/dujulacis/tictacdroid/MainActivity.kt
@@ -1,6 +1,7 @@
 package dujulacis.tictacdroid
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,6 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun boardTapped(view: View) {}
 }
